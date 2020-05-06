4626451152a465afba7a4a9aaf60b6ab4719f16c
Maciej Poreba <macpor9@gmail.com>
2020-05-07 01:49:25 +0200

    Add function to buy product, change calculator

--- logic.py
+++ logic.py
@@ -28,37 +28,52 @@ class Product():
     def __repr__(self):
         return self.__str__()
 
-
 class Automat():
     def __init__(self):
-        self.__moneys = {1: 0, 2: 0, 5:0, 10:0, 20:0, 50:0, 100:0, 200:0, 500:0}
-        self.__products = {30:Product("Fanta",10,250),
-                         31:Product("Coca-cola",10,250),
-                         32:Product("Sprite",10,250),
-                         33:Product("Mountain-Dew",10,250),
-                         34:Product("Mirinda",10,250),
-                         35:Product("Pepsi",10,250),
-                         36:Product("7-UP",10,250),
-                         37:Product("Woda Zywiec Zdroj",10,200),
-                         38:Product("Tymbark jablko mieta 0.5L",7,350),
-                         39:Product("Tymbark jablko brzoskwinia 0.5L",7,350),
-                         40:Product("Tymbark pomarancza brzoskwinia 0.5L",7,350),
-                         41:Product("Tiger",10,300),
-                         42:Product("Black",10,300),
+        self.__addedMoney = 0
+        #key -   liczba groszy
+        #value - liczba dostepnych monet o podanej wartosci
+        self.__moneys = {500: 5, 200: 0, 100:0, 50:0, 20:0, 10:0, 5:0, 2:0, 1:0}
+        self.__products = {30:Product("Fanta",5,250),
+                         31:Product("Coca-cola",5,250),
+                         32:Product("Sprite",5,250),
+                         33:Product("Mountain-Dew",5,250),
+                         34:Product("Mirinda",5,250),
+                         35:Product("Pepsi",5,250),
+                         36:Product("7-UP",5,250),
+                         37:Product("Woda Zywiec Zdroj",5,200),
+                         38:Product("Tymbark jablko mieta 0.5L",5,350),
+                         39:Product("Tymbark jablko brzoskwinia 0.5L",5,350),
+                         40:Product("Tymbark pomarancza brzoskwinia 0.5L",5,350),
+                         41:Product("Tiger",5,300),
+                         42:Product("Black",5,300),
                          43:Product("Oshee blue 0.5L",5,400),
                          44:Product("Oshee red 0.5L",5,400),
                          45:Product("Oshee yellow 0.5L",5,400),
-                         46:Product("Lipton ice tea",10,300),
+                         46:Product("Lipton ice tea",5,300),
                          47:Product("Oshee blue 0.5L ZERO", 5, 400),
                          48:Product("Oshee red 0.5L ZERO", 5, 400),
                          49:Product("Oshee yellow 0.5L ZERO", 5, 400),
-                         50:Product("Lipton ice tea ZERO", 10, 300),
+                         50:Product("Lipton ice tea ZERO", 5, 300),
                          }
+        self.cantGiveTheChange = None
+        self.cantGiveTheChange = None
+        self.viewPrice = None
+        self.productNotAvailable = None
+        self.viewTransaction = None
+
+
+    def setFuncitons(self,viewPrice, productNotAvailable,viewTransaction,cantGiveTheChange):
+        self.cantGiveTheChange = cantGiveTheChange
+        self.viewPrice = viewPrice
+        self.productNotAvailable = productNotAvailable
+        self.viewTransaction = viewTransaction
 
     def addMoney(self,moneyValue):
+        self.__addedMoney+=moneyValue
         self.__moneys[moneyValue]+=1
 
-    def paidAmout(self):
+    def valueOfAllMoney(self):
         retval = 0
         for k,v in self.__moneys.items():
             retval+=k*v
@@ -67,9 +82,67 @@ class Automat():
     def getProductByID(self,id):
         return self.__products[id]
 
+    def calculateChange(self):
+        while self.__addedMoney>0:
+            n = 0
+            for nominal in self.__moneys.keys():
+                if self.__moneys[nominal]>0:
+                    if self.__addedMoney >= nominal > n:
+                        n = nominal
+            self.__addedMoney-=n
+            self.__moneys[n]-=1
+            if self.valueOfAllMoney()<=0 and self.__addedMoney>0:
+                self.cantGiveTheChange()
+
+    def getAddedMoney(self):
+        return self.__addedMoney
+
+
+
+    def buyProduct(self, id ):
+        if self.__addedMoney<self.getProductByID(id).getPrice():
+            self.viewPrice(id)
+            self.productNotAvailable()
+            print("war1")
+            return
+        if self.getProductByID(id).getCount()<=0:
+            self.productNotAvailable()
+            print("war2")
+            return
+        self.calculateChange()
+        self.getProductByID(id).setCount(self.getProductByID(id).getCount()-1)
+        self.viewTransaction()
+
+
+def cantGiveTheChange():
+    print("tylko odliczona kwota")
+
+def viewPrice(id):
+    print(a.getProductByID(id))
+
+def productNotAvailable():
+    print("product not available")
+
+def viewTransaction():
+    print("kupiono produkt: ")
+
+
+
+
 a = Automat()
-print(a.getProductByID(35))
-a.addMoney(200)
+a.setFuncitons(viewPrice,productNotAvailable,viewTransaction,cantGiveTheChange)
+a.addMoney(5)
+a.addMoney(100)
 a.addMoney(500)
-a.addMoney(20)
-print("paid amount",a.paidAmout())
+
+
+print("added money", a.getAddedMoney())
+print("money value in machine", a.valueOfAllMoney())
+
+a.buyProduct(41)
+
+print(a.getProductByID(41))
+print("added money", a.getAddedMoney())
+print("money value in machine", a.valueOfAllMoney())
+
+
